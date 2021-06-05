723e2349a367d177adc296baaf76e6c6563290d7
Dave H <jahorse@gmail.com>
2021-06-05 16:02:05 -0500

    Added function to determine scp target with least amount of current transfers

--- plotmanager/library/utilities/create-chia-plot.sh
+++ plotmanager/library/utilities/create-chia-plot.sh
@@ -2,11 +2,36 @@
 
 set -eou pipefail
 
+get_target() {
+    declare -A target_count
+
+    avaiable_targets=$(cat ~/.ssh/config | grep "Host plotTarget" | grep -o "plotTarget[a-zA-Z0-9]*")
+
+    lowest_amount=1000
+    lowest_target=${avaiable_targets[0]}
+    for t in ${avaiable_targets[@]}; do
+        set +e
+        target_count[$t]=$(ps aux | grep scp | grep -v grep | grep -v /usr/bin/ssh | grep -c $t:)
+        set -e
+        if [ -z ${target_count[$t]+x} ]; then
+            target_count[$t]=0
+        fi
+
+        if (( ${target_count[$t]} < $lowest_amount )); then
+            lowest_amount=${target_count[$t]}
+            lowest_target=$t
+        fi
+    done
+
+    echo $lowest_target
+}
+
 migrate_plot() {
     plot_name_full=$1
-    remote_destination=$(ssh plotTarget "python3 get-farm-drive.py")
+    plot_target=$(get_target)
+    remote_destination=$(ssh $plot_target "python3 get-farm-drive.py")
     echo "Uploading $plot_name_full to $remote_destination"
-    scp "$plot_name_full" plotTarget:$remote_destination
+    scp "$plot_name_full" $plot_target:$remote_destination
     echo "Finished uploading $plot_name_full"
 
     echo "Deleting $plot_name_full"
